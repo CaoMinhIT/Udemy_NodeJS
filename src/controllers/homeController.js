76f962e89afec7b3d10422f7b477489b43a99cff
ZipZip <xuatphat18@gmail.com>
2023-02-25 10:19:21 +0700

    25/2/2023

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -8,20 +8,18 @@ const getHomepage = (req,res) =>{
 
 const getData = (req, res) =>{
     let users = [];
-    
+    res.render("data.ejs");
     connection.query(
         'SELECT * FROM Users u',
             function(err, results, fields) {
                 users = results;
                 console.log(">>>results", results); 
-
                 // console.log (">> check users", users)
                 res.send(JSON.stringify(users));
         }
     );
 }
 const getABC = (req,res) =>{
-
     res.render('sample.ejs');
 } 
 
@@ -29,29 +27,31 @@ const postCreateUser = (req,res) => {
     let email = req.body.email; 
     let name = req.body.name;
     let city = req.body.city;
-        // OR let {email, name, city} = req.body;
 
-    console.log (">>email ", email,">>name ", name,">>city ", city);
-    console.log(">>> req.body", req.body);
+        // Comment help you to know what come
+            console.log (">>email ", email,">>name ", name,">>city ", city);
+            console.log(">>> req.body", req.body);
     
-
+    // Normal insert
     connection.query(
         `INSERT INTO Users (email, name, city) 
         VALUES (?,?, ?)`,
         [email , name, city],
         function(err, results) {
-
             console.log(results);
-
             res.send("Create a new user successed");
         }
     );
+}
 
+const getCreatePage = (req,res) =>{
+    res.render('create.ejs');
 }
 
 module.exports = {
     getHomepage,
     getABC,
     getData,
-    postCreateUser
+    postCreateUser,
+    getCreatePage
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -6,12 +6,13 @@ const {getHomepage} = require('../controllers/homeController')
 const {getABC} = require('../controllers/homeController')
 const {getData} = require('../controllers/homeController')
 const {postCreateUser} = require('../controllers/homeController')
+const {getCreatePage} = require('../controllers/homeController')
 
 router.get('/', getHomepage)
 router.get('/hello', getABC )
 router.get('/data', getData)
 
-
 router.post('/create-user',postCreateUser)
+router.get('/create', getCreatePage)
 
 module.exports = router;
